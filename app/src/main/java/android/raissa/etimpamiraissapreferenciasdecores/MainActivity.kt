1283a37963026d8817d861c7f9499dbe97b9863c
Raissa Santos22 <raissa.santos171@etec.sp.gov.br>
2025-05-30 09:55:08 -0300

    Aplicativo terminado,testado e está funcionando conforme orientado

--- app/src/main/java/android/raissa/etimpamiraissapreferenciasdecores/MainActivity.kt
+++ app/src/main/java/android/raissa/etimpamiraissapreferenciasdecores/MainActivity.kt
@@ -4,22 +4,29 @@ import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Bundle
 import android.raissa.etimpamiraissapreferenciasdecores.databinding.ActivityMainBinding
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
-    private lateinit var preferencias: SharedPreferences
+    lateinit var binding : ActivityMainBinding
+
+    companion object{
+        const val NOME_ARQUIVO = "arquivo.prefs.xml"
+    }
+
+    private var cor = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
         super.onCreate(savedInstanceState)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
-        var cor = ""
 
         binding.bntCor1.setOnClickListener{
             cor = "#FD2A2A"
@@ -47,8 +54,43 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        val preferencias = getSharedPreferences(NOME_ARQUIVO, MODE_PRIVATE)
+        val cor = preferencias.getString("cor", "")
+
+        if(cor!!.isNotEmpty()){
+            binding.layoutPrincipal.setBackgroundColor(Color.parseColor(cor))
+        }
+    }
+
     private fun salvarCor(cor:String){
+
         binding.layoutPrincipal.setBackgroundColor(Color.parseColor(cor))
+
+        binding.bntTrocar.setOnClickListener {view ->
+            var preferencias = getSharedPreferences(NOME_ARQUIVO, MODE_PRIVATE)
+            var editor = preferencias.edit()
+            editor.putString("cor", cor)
+            editor.putString("nome","Raissa")
+            editor.putString("sobrenome", "Santos")
+            editor.putInt("idade", 17)
+            editor.apply()
+
+            snackBar(view)
+        }
+    }
+
+    private fun snackBar(view:View){
+        val snackbar = Snackbar.make(view, "Cor de fundo trocada com sucesso!", Snackbar.LENGTH_INDEFINITE)
+        snackbar.setAction("OK"){
+
+        }
+        snackbar.setActionTextColor(Color.BLUE)
+        snackbar.setBackgroundTint(Color.LTGRAY)
+        snackbar.setTextColor(Color.GREEN)
+        snackbar.show()
     }
 
 }
